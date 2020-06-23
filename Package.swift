7569a8435e08e82578ca1f573d06b566bf76e103
Cody Garvin <emig647@gmail.com>
2020-06-23 15:38:25 -0700

    Attempt at dynamic

--- Package.swift
+++ Package.swift
@@ -5,11 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "Analytics",
+    platforms: [
+        .iOS(.v12), .tvOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Analytics",
-            type: .static,
+            type: .dynamic,
             targets: ["Analytics"]),
     ],
     dependencies: [
