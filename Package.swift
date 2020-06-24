d043540e40d04b9b754952cbebbf8212e12a7fb0
Cody Garvin <emig647@gmail.com>
2020-06-23 17:15:34 -0700

    Update

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Analytics",
-            type: .dynamic,
+            type: .none,
             targets: ["Analytics"]),
     ],
     dependencies: [
@@ -32,10 +32,10 @@ let package = Package(
             cSettings: [
                 .headerSearchPath("Internal"),
                 .headerSearchPath("Classes")
-            ],
-            linkerSettings: [
-                .unsafeFlags(["-fprofile-instr-generate"]),
             ]
+//            linkerSettings: [
+//                .unsafeFlags(["-fprofile-instr-generate"]),
+//            ]
         ),
     ]
 )
